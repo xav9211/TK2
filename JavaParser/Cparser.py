fa8826092e9a5cecaffc7e26259d93a21ddb5827
xav9211 <ksawery.glaz@gmail.com>
2014-04-26 16:43:32 +0200

    parser and scanner

--- JavaParser/Cparser.py
+++ JavaParser/Cparser.py
@@ -3,8 +3,6 @@
 from scanner import Scanner
 import AST
 
-
-
 class Cparser(object):
 
 
@@ -14,25 +12,43 @@ class Cparser(object):
 
     tokens = Scanner.tokens
 
-
-    precedence = (
-       ("nonassoc", 'IFX'),
-       ("nonassoc", 'ELSE'),
-       ("right", '='),
-       ("left", 'OR'),
-       ("left", 'AND'),
-       ("left", '|'),
-       ("left", '^'),
-       ("left", '&'),
-       ("nonassoc", '<', '>', 'EQ', 'NEQ', 'LE', 'GE'),
-       ("left", 'SHL', 'SHR'),
-       ("left", '+', '-'),
-       ("left", '*', '/', '%'),
-    )
-
+    # precedence = (
+    #    ("nonassoc", 'IFX'),
+    #    ("nonassoc", 'ELSE'),
+    #    ("right", '='),
+    #    ("left", 'OR'),
+    #    ("left", 'AND'),
+    #    ("left", '|'),
+    #    ("left", '^'),
+    #    ("left", '&'),
+    #    ("nonassoc", '<', '>', 'EQ', 'NEQ', 'LE', 'GE'),
+    #    ("left", 'SHL', 'SHR'),
+    #    ("left", '+', '-'),
+    #    ("left", '*', '/', '%'),
+    # )
 
     def p_error(self, p):
         if p:
             print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, self.scanner.find_tok_column(p), p.type, p.value))
         else:
             print('At end of input')
+
+    def p_program(self, p):
+        """program : ACCESS TYPE ID inherited BODY"""
+
+    def p_inherited(self, p):
+        """inherited : extend implements
+                    | implements extend
+                    | extend
+                    | implements
+                    | """
+
+    def p_extend(self, p):
+        """extend : EXTENDS ID"""
+
+    def p_implements(self, p):
+        """implements : implements implement
+                    | """
+
+    def implement(self, p):
+        """implement : IMPLEMENTS ID"""
--- JavaParser/scanner.py
+++ JavaParser/scanner.py
@@ -4,61 +4,73 @@ import ply.lex as lex
 class Scanner(object):
 
 
-  def find_tok_column(self, token):
-      last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
-      if last_cr < 0:
-        last_cr = 0
-      return token.lexpos - last_cr
+    def find_tok_column(self, token):
+        last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
+        if last_cr < 0:
+            last_cr = 0
+        return token.lexpos - last_cr
 
 
-  def build(self):
-      self.lexer = lex.lex(object=self)
+    def build(self):
+        self.lexer = lex.lex(object=self)
 
-  def input(self, text):
-      self.lexer.input(text)
+    def input(self, text):
+        self.lexer.input(text)
 
-  def token(self):
-      return self.lexer.token()
-
-
-
-  literals = "{}()<>=;:,+-*/%&|^"
+    def token(self):
+        return self.lexer.token()
 
 
-  reserved = {
-   'class'     : 'CLASS',
-   'interface' : 'INTERFACE',
-   'implements': 'IMPLEMENTS',
-   'extends'   : 'EXTENDS',
-   'private'   : 'PRIVATE',
-   'protected' : 'PROTECTED',
-   'public'    : 'PUBLIC',
-  }
+
+    literals = "{}()<>=;:,+-*/%&|^"
 
 
-  tokens = [   ] + list(reserved.values())
-           
-
-  t_ignore = ' \t\f'
-
-  def t_newline(self,t):
-      r'\n+'
-      t.lexer.lineno += len(t.value)
-
-  def t_newline2(self,t):
-      r'(\r\n)+'
-      t.lexer.lineno += len(t.value) / 2
+    reserved = {
+    'implements' : 'IMPLEMENTS',
+    'extends'   : 'EXTENDS',
+    }
 
 
-  def t_error(self,t):
-      print("Illegal character '{0}' ({1}) in line {2}".format(t.value[0], hex(ord(t.value[0])), t.lexer.lineno))
-      t.lexer.skip(1)
+    tokens = [ "ACCESS", "TYPE", "ID", "BODY:",  ] + list(reserved.values())
 
 
-  def t_LINE_COMMENT(self,t):
-      r'\#.*'
-      pass
+    t_ignore = ' \t\f'
 
-  def t_BLOCK_COMMENT(self,t):
-      r'/\*(.|\n)*?\*/'
-      t.lexer.lineno += t.value.count('\n')
+    def t_newline(self,t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    def t_newline2(self,t):
+        r'(\r\n)+'
+        t.lexer.lineno += len(t.value) / 2
+
+
+    def t_error(self,t):
+        print("Illegal character '{0}' ({1}) in line {2}".format(t.value[0], hex(ord(t.value[0])), t.lexer.lineno))
+        t.lexer.skip(1)
+
+
+    def t_LINE_COMMENT(self,t):
+        r'\#.*'
+        pass
+
+    def t_BLOCK_COMMENT(self,t):
+        r'/\*(.|\n)*?\*/'
+        t.lexer.lineno += t.value.count('\n')
+
+    def t_ACCESS(self, t):
+        r"\b(private|protected|public)\b"
+        return t
+
+    def t_TYPE(self, t):
+        r"\b((abstract? class)|interface)\b"
+        return t
+
+    def t_ID(self, t):
+        r"[a-zA-Z_]\w*"
+        t.type = Scanner.reserved.get(t.value, 'ID')
+        return t
+
+    def t_BODY(self, t):
+        r"{\w*}"
+        return t
